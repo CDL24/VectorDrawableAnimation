a99981b92ce44f58c0712daff9f5fdd24b0357bf
C.limbachiya <limbachiya24@gmail.com>
2016-04-06 16:52:50 +0530

    second commit

--- app/src/main/java/com/example/climbachiya/glikeanimation/FragmentVector.java
+++ app/src/main/java/com/example/climbachiya/glikeanimation/FragmentVector.java
@@ -1,8 +1,16 @@
 package com.example.climbachiya.glikeanimation;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,10 +28,11 @@ import android.widget.RelativeLayout;
  */
 public class FragmentVector extends Fragment {
 
-    private Animation btnAnim;
+    private Animation btnAnim, scaleAnim;
     RelativeLayout layout;
-    ImageView imgReward,imgRewardClone;
+    ImageView imgReward,imgRewardClone, imgHeart;
     boolean isLiked = false;
+    boolean isHearted = false;
     MediaPlayer mp = null;
 
     @Nullable
@@ -39,6 +48,9 @@ public class FragmentVector extends Fragment {
         layout = (RelativeLayout) view.findViewById(R.id.root);
         imgReward = (ImageView) view.findViewById(R.id.imgReward);
         imgRewardClone =(ImageView) view.findViewById(R.id.imgRewardClone);
+        imgHeart =(ImageView) view.findViewById(R.id.image_heart);
+
+        changeBackground();
 
         loadAndSetAnimation();
 
@@ -48,6 +60,29 @@ public class FragmentVector extends Fragment {
                 startAnimation();
             }
         });
+        imgHeart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimationHeart();
+            }
+        });
+    }
+
+    private void startAnimationHeart() {
+
+        imgHeart.startAnimation(scaleAnim);
+
+    }
+
+    private void changeBackground() {
+
+        int colorFrom = Color.WHITE;
+        int colorTo = ContextCompat.getColor(getActivity(), R.color.colorsky);
+        int duration = 1000;
+        ObjectAnimator.ofObject(layout, "backgroundColor", new ArgbEvaluator(), colorFrom, colorTo)
+                .setDuration(duration)
+                .start();
+
     }
 
     //Start actual animation here
@@ -64,8 +99,45 @@ public class FragmentVector extends Fragment {
      */
     private void loadAndSetAnimation() {
 
+        scaleAnim = AnimationUtils.loadAnimation(getActivity(), R.anim.scale_heart);
+        scaleAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
-        btnAnim = AnimationUtils.loadAnimation(getActivity(), R.anim.btn_anim);
+                /*AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(),
+                        R.animator.property_animator);
+                set.setTarget(imgHeart);
+                set.start();*/
+
+                //animateImageView();
+                mp = MediaPlayer.create(getActivity(), R.raw.fb_click);
+                mp.start();
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mp.release();
+                int color;
+                if(!isHearted){
+                    color = R.color.colorAccent;
+                }else{
+                    color = R.color.colorBlack;
+                }
+
+                isHearted = !isHearted;
+                PorterDuffColorFilter porterDuffColorFilter = new PorterDuffColorFilter(ContextCompat.getColor(getActivity(), color),
+                        PorterDuff.Mode.SRC_ATOP);
+
+                imgHeart.setColorFilter(porterDuffColorFilter);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        btnAnim = AnimationUtils.loadAnimation(getActivity(), R.anim.like_animation);
         btnAnim.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -95,5 +167,36 @@ public class FragmentVector extends Fragment {
 
             }
         });
+    }
+
+    private void animateImageView() {
+
+        final int color = ContextCompat.getColor(getActivity(), R.color.colorAccent);
+
+        final ValueAnimator colorAnim = ObjectAnimator.ofFloat(0f, 1f);
+        colorAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float mul = (Float) animation.getAnimatedValue();
+                int alphaOrange = adjustAlpha(color, mul);
+                imgHeart.setColorFilter(alphaOrange, PorterDuff.Mode.SRC_ATOP);
+                if (mul == 0.0) {
+                    imgHeart.setColorFilter(null);
+                }
+            }
+        });
+
+        colorAnim.setDuration(1500);
+        colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnim.setRepeatCount(-1);
+        colorAnim.start();
+    }
+
+    public int adjustAlpha(int color, float factor) {
+        int alpha = Math.round(Color.alpha(color) * factor);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        return Color.argb(alpha, red, green, blue);
     }
 }
--- app/src/main/java/com/example/climbachiya/glikeanimation/FragmentVectorAnimation.java
+++ app/src/main/java/com/example/climbachiya/glikeanimation/FragmentVectorAnimation.java
@@ -1,12 +1,24 @@
 package com.example.climbachiya.glikeanimation;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Fragment;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * Created by C.limbachiya on 4/5/2016.
@@ -14,6 +26,7 @@ import android.widget.ImageView;
 public class FragmentVectorAnimation extends Fragment {
 
     ImageView imgTruck;
+    RelativeLayout llRoot;
 
     @Nullable
     @Override
@@ -25,8 +38,67 @@ public class FragmentVectorAnimation extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        llRoot = (RelativeLayout) view.findViewById(R.id.root);
         imgTruck = (ImageView) view.findViewById(R.id.imgTruck);
+
+        changeBackground();
+
+        /*AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(),
+                R.animator.property_animator);
+        set.setTarget(imgTruck);
+        set.start();*/
+
+        //loading xml from anim folder
+        //Animation transAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.truck_tanslate);
+        //You can now apply the animation to a view
+        //imgTruck.startAnimation(transAnimation);
+
+        /*RotateAnimation rAnim = new RotateAnimation(0, 359, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rAnim.setDuration(1000);
+        imgTruck.startAnimation(rAnim);*/
+
+        animateImageView(imgTruck);
     }
 
+    private void changeBackground() {
 
+        int colorFrom = Color.WHITE;
+        int colorTo = ContextCompat.getColor(getActivity(), R.color.colorlightgreen);
+        int duration = 1000;
+        ObjectAnimator.ofObject(llRoot, "backgroundColor", new ArgbEvaluator(), colorFrom, colorTo)
+                .setDuration(duration)
+                .start();
+
+    }
+
+    private void animateImageView(final ImageView imgView) {
+
+        final int color = ContextCompat.getColor(getActivity(), R.color.colorAccent);
+
+        final ValueAnimator colorAnim = ObjectAnimator.ofFloat(0f, 1f);
+        colorAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float mul = (Float) animation.getAnimatedValue();
+                int alphaOrange = adjustAlpha(color, mul);
+                imgView.setColorFilter(alphaOrange, PorterDuff.Mode.SRC_ATOP);
+                if (mul == 0.0) {
+                    imgView.setColorFilter(null);
+                }
+            }
+        });
+
+        colorAnim.setDuration(1500);
+        colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnim.setRepeatCount(-1);
+        colorAnim.start();
+    }
+
+    public int adjustAlpha(int color, float factor) {
+        int alpha = Math.round(Color.alpha(color) * factor);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        return Color.argb(alpha, red, green, blue);
+    }
 }
